3fc234523c3cac2392dffc38e46dd677e7a0211a
Ryan Pickell <rianchristopher@gmail.com>
2022-12-28 17:55:24 -0800

    implemented exception handling for api timeouts and server errors

--- main.py
+++ main.py
@@ -53,8 +53,17 @@ class BookSearch:
 
     def fetch_books(self, search_term):
         """ Fetches books from API. """
-        response = requests.get("https://www.googleapis.com/books/v1/volumes?q=" + search_term)
-        response.raise_for_status()
+        try:
+            response = requests.get("https://www.googleapis.com/books/v1/volumes?q=" + search_term)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as errh:
+            return "An Http Error occurred:" + repr(errh)
+        except requests.exceptions.ConnectionError as errc:
+            return "An Error Connecting to the API occurred:" + repr(errc)
+        except requests.exceptions.Timeout as errt:
+            return "A Timeout Error occurred:" + repr(errt)
+        except requests.exceptions.RequestException as err:
+            return "An Unknown Error occurred" + repr(err)
 
         parsed_books = response.json()
         if Validation().validate_response(parsed_books):

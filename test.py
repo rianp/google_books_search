4b23b312de37d15b452d5d74d628b8d8a1925d47
Ryan Pickell <rianchristopher@gmail.com>
2022-12-30 20:42:16 -0800

    refactored unittest and started mock test for booksearch class

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import unittest
+from unittest.mock import patch
+import mock
 from main import *
 
 
@@ -18,11 +20,19 @@ class TestValidation(unittest.TestCase):
     def test_invalid_bool_returns_false(self):
         self.assertFalse(self.validation.validate_bool("x"))
 
+    def test_valid_menu_choice_returns_true(self):
+        self.assertTrue(self.validation.validate_menu_choice("s"))
+        self.assertTrue(self.validation.validate_menu_choice("r"))
+        self.assertTrue(self.validation.validate_menu_choice("x"))
+
+    def test_invalid_menu_choice_returns_false(self):
+        self.assertFalse(self.validation.validate_menu_choice("f"))
+
     def test_valid_selection_returns_true(self):
-        self.assertTrue(self.validation.validate_selection(1))
+        self.assertTrue(self.validation.validate_selection(1, 5))
 
     def test_invalid_selection_returns_false(self):
-        self.assertFalse(self.validation.validate_selection(6))
+        self.assertFalse(self.validation.validate_selection(6, 5))
 
     def test_valid_response_returns_true(self):
         ok_response = {'kind': 'books#volumes', 'totalItems': 2, 'items': [{}, {}]}
@@ -33,29 +43,37 @@ class TestValidation(unittest.TestCase):
         self.assertFalse(self.validation.validate_response(bad_response))
 
 
+class TestBookSearch(unittest.TestCase):
+    @patch('main.BookSearch')
+    def test_blog_posts(self, MockSearch):
+        book_search = MockSearch()
+
+        book_search.get_search_term().return_value = [
+            {
+                "Authors": "Author Name",
+                "Title": "My Book Title",
+                "Publisher": "Publishing Co."
+            }
+        ]
+
+        response = book_search.get_search_term()
+        print(response[0])
+        self.assertIsNotNone(response)
+        print(response[0])
+        # self.assertIsInstance(response[0], dict)
+
+
 class TestBook(unittest.TestCase):
     test_book = Book(["Author Name"], "My Book Title", "Publishing Co.")
     test_empty_info_book = Book([], "My Book Title", "")
 
-    def test_get_author_returns_book_author(self):
-        fetched_author = self.test_book.get_author()
-        self.assertEqual(["Author Name"], fetched_author)
+    def test_book_str(self):
+        self.assertEqual(str(self.test_book),
+                         "Authors: Author Name\nTitle: My Book Title\nPublisher: Publishing Co.")
 
-    def test_get_author_returns_empty_string_if_none(self):
-        fetched_author = self.test_empty_info_book.get_author()
-        self.assertEqual([], fetched_author)
-
-    def test_get_title_returns_book_title(self):
-        fetched_title = self.test_book.get_title()
-        self.assertEqual("My Book Title", fetched_title)
-
-    def test_get_publisher_returns_book_publisher(self):
-        fetched_publisher = self.test_book.get_publisher()
-        self.assertEqual("Publishing Co.", fetched_publisher)
-
-    def test_get_publisher_returns_empty_string_if_none(self):
-        fetched_publisher = self.test_empty_info_book.get_publisher()
-        self.assertEqual("", fetched_publisher)
+    def test_book_repr(self):
+        self.assertEqual(str(self.test_book),
+                         "Authors: Author Name\nTitle: My Book Title\nPublisher: Publishing Co.")
 
 
 class TestReadList(unittest.TestCase):

11d2ab57197422b5f4f71e0a82e2c32e43d7a812
Ryan Pickell <rianchristopher@gmail.com>
2023-01-05 15:31:45 -0800

    added print functionality to excpetion handling in apifetch

--- api_fetch.py
+++ api_fetch.py
@@ -1,4 +1,5 @@
 import requests
+from console import *
 
 class APIFetch:
 
@@ -9,12 +10,16 @@ class APIFetch:
             response = requests.get("https://www.googleapis.com/books/v1/volumes?q=" + search_term)
             response.raise_for_status()
         except requests.exceptions.HTTPError as errh:
-            return "An Http Error occurred:" + repr(errh)
+            Console.print_string(f"An Http Error occurred: {repr(errh)}")
+            return
         except requests.exceptions.ConnectionError as errc:
-            return "An Error Connecting to the API occurred:" + repr(errc)
+            Console.print_string(f"An Error Connecting to the API occurred: {repr(errc)}")
+            return
         except requests.exceptions.Timeout as errt:
-            return "A Timeout Error occurred:" + repr(errt)
+            Console.print_string(f"A Timeout Error occurred: {repr(errt)}")
+            return
         except requests.exceptions.RequestException as err:
-            return "An Unknown Error occurred" + repr(err)
+            Console.print_string(f"An Unknown Error occurred {repr(err)}")
+            return
 
         return response.json()
